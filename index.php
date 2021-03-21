7e7b20b12ab53d965fda936ca7ab3a00e7bfa294
Gabriel Tosetti <tosetti.gabriel@gmail.com>
2021-03-20 23:37:19 -0300

    error reporting, time execution

--- index.php
+++ index.php
@@ -3,21 +3,8 @@ include_once 'vendor/autoload.php';
 
 use \Cnab\Retorno\Factory;
 
-function getMemoryPeak()
-{
-    $bytes = memory_get_peak_usage();
-    $precision = 2;
-
-    $units = array("b", "kb", "mb", "gb", "tb");
-
-    $bytes = max($bytes, 0);
-    $pow = floor(($bytes ? log($bytes) : 0) / log(1024));
-    $pow = min($pow, count($units) - 1);
-
-    $bytes /= (1 << (10 * $pow));
-
-    return round($bytes, $precision) . " " . $units[$pow];
-}
+error_reporting(E_ALL);
+set_time_limit(1000);
 
 print "\nMemory: " . getMemoryPeak() . "\n\n";
 
